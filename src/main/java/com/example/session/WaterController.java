af2684362cd929040195bc6b3136530e96a4011d
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2020-12-19 10:59:30 +0100

    created WaterController

--- src/main/java/com/example/session/WaterController.java
+++ src/main/java/com/example/session/WaterController.java
@@ -1,13 +1,35 @@
 package com.example.session;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
-@WebServlet("")
+import java.io.IOException;
+
+@WebServlet("/control")
 public class WaterController extends HttpServlet {
+    private static final int GLASS_OF_WATER = 250;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String option = request.getParameter("option");
+        HttpSession session = request.getSession();
+        if(option.equals("water")){
+            increaseWaterLevel(session);
+        }else if (option.equals("clear")){
+            session.removeAttribute("water");
+        }
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
+    }
 
+    private void increaseWaterLevel(HttpSession session){
+        Object water = session.getAttribute("water");
+        int resultWater;
+        if(water == null){
+            resultWater = GLASS_OF_WATER;
+        } else {
+            resultWater = ((int)water) + GLASS_OF_WATER;
+        }
+        session.setAttribute("water", resultWater);
     }
 
 }
